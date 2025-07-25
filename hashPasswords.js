301ae0b95ef6a0e15321aae982fcb065cf94d7ef
Bobomurod Xudayberganov <khudayberganovbobomurod@gmail.com>
2025-07-25 16:31:56 +0500

    edited

--- hashPasswords.js
+++ hashPasswords.js
@@ -1,24 +1,29 @@
-const fs = require('fs').promises;
-const bcrypt = require('bcrypt');
+const fs = require('fs').promises
+const bcrypt = require('bcryptjs')
 
 const hashPasswords = async () => {
-  try {
-    const data = await fs.readFile('users.json', 'utf8');
-    const users = JSON.parse(data);
+	try {
+		const data = await fs.readFile('users.json', 'utf8')
+		const users = JSON.parse(data)
 
-    const saltRounds = 10;
-    for (let user of users) {
-      if (!user.password.startsWith('$2b$')) { // Agar parol shifrlanmagan bo‘lsa
-        const hashedPassword = await bcrypt.hash(user.password, saltRounds);
-        user.password = hashedPassword;
-      }
-    }
+		const saltRounds = 10
+		for (let user of users) {
+			if (!user.password.startsWith('$2b$')) {
+				// Agar parol shifrlanmagan bo‘lsa
+			const hashedPassword = bcrypt.hashSync(password, saltRounds);
+				user.password = hashedPassword
+			}
+		}
 
-    await fs.writeFile('users.json', JSON.stringify(users, null, 2));
-    console.log('Parollar shifrlangan!');
-  } catch (error) {
-    console.error('Xato:', error);
-  }
-};
+		await fs.writeFile('users.json', JSON.stringify(users, null, 2))
+		console.log('Parollar shifrlangan!')
+	} catch (error) {
+		console.error('Xato:', error)
+	}
+}
 
-hashPasswords();
+hashPasswords()
+
+
+
+
